7b7b7a96e6a5793741342bae1587346e53867dfb
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-11 16:21:54 +0600

    create and read operation

--- index.js
+++ index.js
@@ -27,9 +27,18 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        app.post('/users', async(req, res)=> {
+        const userCollection = client.db('usersDB').collection('users');
+
+        app.get('/users', async(req, res)=> {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
         })
 
 
